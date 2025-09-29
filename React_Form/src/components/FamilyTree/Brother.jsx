fc5de9c849ad0095d4d1156a05a7e02342b174d8
antnose <antnose28@gmail.com>
2025-09-29 23:56:51 +0600

    Using context api update the valo

--- React_Form/src/components/FamilyTree/Brother.jsx
+++ React_Form/src/components/FamilyTree/Brother.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { use } from "react";
+import { MoneyContext } from "./FamilyTree";
 
 const Brother = () => {
+  const [money, setMoney] = use(MoneyContext);
+
   return (
     <div>
       <h3>Brother</h3>
+      <button className="btn mt-4" onClick={() => setMoney(money + 1000)}>
+        Add TK
+      </button>
     </div>
   );
 };
--- React_Form/src/components/FamilyTree/FamilyTree.jsx
+++ React_Form/src/components/FamilyTree/FamilyTree.jsx
@@ -1,17 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Grandpa from "./Grandpa";
 
 export const AssetsContext = createContext("");
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
   const asset = "Diamond";
   const newAsset = "Gold";
+  const [money, setMoney] = useState(0);
+
   return (
     <div className="text-center mt-11">
       <h1 className="text-4xl font-bold mb-7">This Is Family Tree</h1>
-      <AssetsContext.Provider value={newAsset}>
-        <Grandpa asset={asset} />
-      </AssetsContext.Provider>
+      <p className="text-2xl font-bold mb-7">Totally family money: {money}</p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetsContext.Provider value={newAsset}>
+          <Grandpa asset={asset} />
+        </AssetsContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
